80b1d834a6a2085017adf6c11dc0e0428798eb94
David Reese <david@reesedevelopment.com>
2021-04-04 09:37:15 +0300

    status bar

---
+++ Offline Alerter/AppDelegate.swift
@@ -0,0 +1,50 @@
+//
+//  AppDelegate.swift
+//  Offline Alerter
+//
+//  Created by David Reese on 4/4/21.
+//
+
+import Cocoa
+import SwiftUI
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Create the SwiftUI view that provides the window contents.
+        
+        let contentView = ContentView()
+
+        // Create the popover
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 400, height: 500)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        self.popover = popover
+        
+        // Create the status item
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(systemSymbolName: "dot.radiowaves.right", accessibilityDescription: nil)
+            button.action = #selector(togglePopover(_:))
+        }
+    }
+    
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+        }
+    }
+
+
+}
+
